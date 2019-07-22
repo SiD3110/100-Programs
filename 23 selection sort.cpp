6486e9675394121230b207567db1288448fb593d
Siddharth Deulkar <42211976+SiD3110@users.noreply.github.com>
2019-07-23 01:42:16 +0530

    Create 23 selection sort.cpp

---
+++ 23 selection sort.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cin>>n;
+	int a[n];
+	for(int i=0;i<n;i++)
+	{
+		cin>>a[i];
+	}
+	for(int i=0;i<n;i++)
+	{
+		int min=a[i],p=i;
+		for(int j=i+1;j<n;j++)
+		{
+			if(a[j]<min)
+			{
+				min=a[j];
+				p=j;
+			}
+		}
+		int t=a[i];
+		a[i]=a[p];
+		a[p]=t;
+	}
+	for(int i=0;i<n;i++)cout<<a[i]<<" ";
+	return 0;
+}
